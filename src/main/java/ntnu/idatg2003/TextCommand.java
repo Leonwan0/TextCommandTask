a6899215a82d9a488a2747c4ca4e240a60571987
Leon <leonwan@stud.ntnu.no>
2026-02-01 16:02:38 +0100

    Add TextCommand interface for text processing commands

---
+++ src/main/java/ntnu/idatg2003/TextCommand.java
@@ -0,0 +1,16 @@
+package ntnu.idatg2003;
+
+/**
+ * An interface representing a text command that can be executed on a given text.
+ * The execute method takes a String input and returns a modified String output.
+ * This interface can be implemented to create various text processing commands.
+ *
+ * @version 1.0
+ * @since 1.0
+ *
+ * @author Leon Wang
+ *
+ */
+public interface TextCommand {
+    String execute(String text);
+}
