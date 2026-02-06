03f069eb42ebb8310640f199aa49c07095dfcd39
Leon <leonwan@stud.ntnu.no>
2026-02-06 17:06:34 +0100

    Add CapitalizeWordsTextCommand to capitalize the first initial of each word

---
+++ src/main/java/ntnu/idatg2003/CapitalizeWordsTextCommand.java
@@ -0,0 +1,15 @@
+package ntnu.idatg2003;
+
+/**
+ * A command that capitalizes the first initial of each word in the input text.
+ * This class extends the CapitalizeTextCommand and provides an implementation
+ * of the execute method to convert the first character of each word in the input text to uppercase,
+ * while leaving the rest of the text unchanged.
+ *
+ * @version 1.0
+ * @since 1.0
+ * @author Leon Wang
+ */
+public class CapitalizeWordsTextCommand extends CapitalizeTextCommand{
+
+}
