8526397a7fd194e62afc9080d27e1c87712d67a0
Leon <leonwan@stud.ntnu.no>
2026-02-06 16:42:53 +0100

    Demonstrate usage of WrapSelectionTextCommand and WrapLinesTextCommand in Main.java

--- src/main/java/ntnu/idatg2003/Main.java
+++ src/main/java/ntnu/idatg2003/Main.java
@@ -8,6 +8,13 @@ public class Main {
 
         TextCommand command2 = new ReplaceFirstTextCommand("target", "replacement");
 
+        WrapSelectionTextCommand wrapSelectionCommand = new WrapSelectionTextCommand("selected text",
+                "<b>", "</b>");
+        System.out.println(wrapSelectionCommand.execute("<b>", "</b>"));
+
+        WrapLinesTextCommand wrapLinesTextCommand = new WrapLinesTextCommand("<b>", "</b>");
+        System.out.println(wrapLinesTextCommand.execute("Line 1\nLine 2\nLine 3"));
+
         System.out.println(command1.execute("This is the target text with multiple target occurrences of the target word."));
 
         System.out.println(command2.execute("This is the target text with multiple target occurrences of the target word."));
