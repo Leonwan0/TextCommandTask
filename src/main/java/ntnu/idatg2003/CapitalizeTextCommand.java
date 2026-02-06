8c1b75adaea1cae80711b7bd18a9da4d90a8c891
Leon <leonwan@stud.ntnu.no>
2026-02-06 16:50:36 +0100

    Implement CapitalizeTextCommand to capitalize input text and handle null input

--- src/main/java/ntnu/idatg2003/CapitalizeTextCommand.java
+++ src/main/java/ntnu/idatg2003/CapitalizeTextCommand.java
@@ -1,5 +1,28 @@
 package ntnu.idatg2003;
 
-public class CapitalizeTextCommand {
+/**
+ * A command that capitalizes all characters in the given text.
+ * This class implements the TextCommand interface and provides an implementation
+ * of the execute method to convert the input text to uppercase.
+ *
+ * @version 1.0
+ * @since 1.0
+ * @author Leon Wang
+ */
+public class CapitalizeTextCommand implements TextCommand {
+    /**
+     * Executes the command to capitalize all characters in the given text.
+     *
+     * @param text The input text to be capitalized. Must not be null.
+     * @return The capitalized version of the input text.
+     * @throws IllegalArgumentException if the input text is null.
+     */
+    @Override
+    public String execute(String text) {
+        if (text == null) {
+            throw new IllegalArgumentException("Text cannot be null");
+        }
+        return text.toUpperCase();
+    }
 
 }
