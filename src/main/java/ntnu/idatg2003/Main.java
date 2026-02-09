f9fa03d57afac84fce21d706ce55c119e18ddd46
Leon <leonwan@stud.ntnu.no>
2026-02-09 14:47:43 +0100

    Update Main.java to demonstrate new text command functionalities and script execution

--- src/main/java/ntnu/idatg2003/Main.java
+++ src/main/java/ntnu/idatg2003/Main.java
@@ -1,6 +1,7 @@
 package ntnu.idatg2003;
 
 
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -22,6 +23,21 @@ public class Main {
 
         CapitalizeTextCommand capitalizeTextCommand = new CapitalizeTextCommand();
         System.out.println(capitalizeTextCommand.execute("this is a sample text."));
+
+        CapitalizeWordsTextCommand capitalizeWordsTextCommand = new CapitalizeWordsTextCommand();
+        System.out.println(capitalizeWordsTextCommand.execute("text to be capitalized."));
+
+        CapitalizeSelectionTextCommand capitalizeSelectionCommand = new CapitalizeSelectionTextCommand("selection");
+        System.out.println(capitalizeSelectionCommand.execute("text with a selection and another selection"));
+
+        List<TextCommand> commands = List.of(
+                new ReplaceTextCommand("target", "replacement"),
+                new CapitalizeWordsTextCommand(),
+                new WrapLinesTextCommand("<p>", "</p>")
+        );
+        Script script = new Script(commands);
+        String result = script.execute("This is the target text with multiple target occurrences of the target word.");
+        System.out.println(result);
     }
 
 }
