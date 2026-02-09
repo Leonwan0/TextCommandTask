01e2df6234ab3be383784cc8deeea5c4d840cb98
Leon <leonwan@stud.ntnu.no>
2026-02-09 18:50:53 +0100

    Refactor Main.java for improved readability by formatting command instantiation and execution

--- src/main/java/commands/Main.java
+++ src/main/java/commands/Main.java
@@ -13,30 +13,40 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        TextCommand command1 = new ReplaceTextCommand("target", "replacement");
+        TextCommand command1 = new ReplaceTextCommand(
+                "target", "replacement");
+        TextCommand command2 = new ReplaceFirstTextCommand(
+                "target", "replacement");
 
-        TextCommand command2 = new ReplaceFirstTextCommand("target", "replacement");
+        WrapSelectionTextCommand wrapSelectionCommand = new WrapSelectionTextCommand(
+                "selection", "<b>", "</b>");
+        System.out.println(wrapSelectionCommand.execute(
+                "This is a sample text with a selection to be wrapped."));
 
-        WrapSelectionTextCommand wrapSelectionCommand = new WrapSelectionTextCommand("selection", "<b>", "</b>");
-        System.out.println(wrapSelectionCommand.execute("This is a sample text with a selection to be wrapped."));
-
-        WrapLinesTextCommand wrapLinesCommand = new WrapLinesTextCommand("<p>", "</p>");
+        WrapLinesTextCommand wrapLinesCommand = new WrapLinesTextCommand(
+                "<p>", "</p>");
         System.out.println(wrapLinesCommand.execute("first line\nsecond line"));
 
 
 
-        System.out.println(command1.execute("This is the target text with multiple target occurrences of the target word."));
-
-        System.out.println(command2.execute("This is the target text with multiple target occurrences of the target word."));
+        System.out.println(command1.execute(
+                "This is the target text with multiple target occurrences of the target word."));
+        System.out.println(command2.execute(
+                "This is the target text with multiple target occurrences of the target word."));
 
         CapitalizeTextCommand capitalizeTextCommand = new CapitalizeTextCommand();
-        System.out.println(capitalizeTextCommand.execute("this is a sample text."));
+        System.out.println(capitalizeTextCommand.execute(
+                "this is a sample text."));
 
-        CapitalizeWordsTextCommand capitalizeWordsTextCommand = new CapitalizeWordsTextCommand();
-        System.out.println(capitalizeWordsTextCommand.execute("text to be capitalized."));
+        CapitalizeWordsTextCommand capitalizeWordsTextCommand =
+                new CapitalizeWordsTextCommand();
+        System.out.println(capitalizeWordsTextCommand.execute(
+                "text to be capitalized."));
 
-        CapitalizeSelectionTextCommand capitalizeSelectionCommand = new CapitalizeSelectionTextCommand("selection");
-        System.out.println(capitalizeSelectionCommand.execute("text with a selection and another selection"));
+        CapitalizeSelectionTextCommand capitalizeSelectionCommand =
+                new CapitalizeSelectionTextCommand("selection");
+        System.out.println(capitalizeSelectionCommand.execute(
+                "text with a selection and another selection"));
 
         List<TextCommand> commands = List.of(
                 new ReplaceTextCommand("target", "replacement"),
