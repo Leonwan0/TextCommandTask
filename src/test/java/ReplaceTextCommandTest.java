896e5b62832e38c2aa66e20a708b311914e763a6
Leon <leonwan@stud.ntnu.no>
2026-02-09 15:00:22 +0100

    Add unit tests for ReplaceTextCommand class

---
+++ src/test/java/ReplaceTextCommandTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * Unit tests for the ReplaceTextCommand class.
+ */
+public class ReplaceTextCommandTest {
+}
